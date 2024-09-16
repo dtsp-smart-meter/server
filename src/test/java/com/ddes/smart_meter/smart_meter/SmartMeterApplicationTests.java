0b240db410080b9bed7a1168a4257fdd11c72423
Scott Varns (Capgemini) <scott.varns@capgemini.com>
2024-09-16 16:30:00 +0100

    Added spring template to repo to work from

---
+++ src/test/java/com/ddes/smart_meter/smart_meter/SmartMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddes.smart_meter.smart_meter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
